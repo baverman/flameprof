327bf7296ebda4f04293d513b9ac834d836a1ac6
Anton Bobrov <bobrov@vl.ru>
2017-10-05 14:15:52 +0300

    pytest plugin and ability to run scripts and modules

--- flameprof.py
+++ flameprof.py
@@ -1,21 +1,32 @@
 #!/bin/env python
 from __future__ import division, print_function
 
+import os.path
 import sys
+import time
 import pstats
 import argparse
 import pprint
+import runpy
 
 from struct import Struct
 from hashlib import sha1
 from functools import partial
 from collections import Counter
 from xml.sax.saxutils import escape
+from cProfile import  Profile
 
 version = '0.3'
 
 eprint = partial(print, file=sys.stderr)
 epprint = partial(pprint.pprint, stream=sys.stderr)
+
+DEFAULT_WIDTH = 1200
+DEFAULT_ROW_HEIGHT = 24
+DEFAULT_FONT_SIZE = 12
+DEFAULT_THRESHOLD = 0.1
+DEFAULT_FORMAT = 'svg'
+DEFAULT_LOG_MULT = 1000000
 
 PY2 = sys.version_info[0] == 2
 if PY2:
@@ -56,7 +67,7 @@ def calc_callers(stats):
             roots.append(func)
             calls[('root', func)] = funcs[func]['stat']
 
-    for func, (cc, nc, tt, ct, clist) in stats.items():
+    for func, (_, _, _, _, clist) in stats.items():
         for cfunc, t in clist.items():
             assert (cfunc, func) not in calls
             funcs[cfunc]['calls'].append(func)
@@ -64,6 +75,22 @@ def calc_callers(stats):
             calls[(cfunc, func)] = t
 
     total = sum(funcs[r]['stat'][3] for r in roots)
+    ttotal = sum(funcs[r]['stat'][2] for r in funcs)
+
+    if not (0.8 < total / ttotal < 1.2):
+        eprint('Warning: can not find proper roots, root cumtime is {} but sum tottime is {}'.format(total, ttotal))
+
+    # Try to find suitable root
+    newroot = max((r for r in funcs if r not in roots), key=lambda r: funcs[r]['stat'][3])
+    nstat = funcs[newroot]['stat']
+    ntotal = total + nstat[3]
+    if 0.8 < ntotal / ttotal < 1.2:
+        roots.append(newroot)
+        calls[('root', newroot)] = nstat
+        total = ntotal
+    else:
+        total = ttotal
+
     funcs['root'] = {'calls': roots,
                      'called': [],
                      'stat': (1, 1, 0, total)}
@@ -71,7 +98,7 @@ def calc_callers(stats):
     return funcs, calls
 
 
-def prepare(funcs, calls, threshold=0.0001, h=24, fsize=12, width=1200):
+def prepare(funcs, calls, threshold=0.0001):
     blocks = []
     bblocks = []
     block_counts = Counter()
@@ -193,20 +220,63 @@ def render_svg_section(blocks, maxw, colors, h=24, fsize=12, width=1200, top=0, 
 
 
 def render_svg(blocks, bblocks, maxw, h=24, fsize=12, width=1200):
-    s1, h1 = render_svg_section(blocks, maxw, [COLORS, CCOLORS], h=h,
-                                fsize=fsize, width=width, top=0)
+    if blocks:
+        s1, h1 = render_svg_section(blocks, maxw, [COLORS, CCOLORS], h=h,
+                                    fsize=fsize, width=width, top=0)
+        h1 += h
+    else:
+        s1, h1 = [], 0
     s2, h2 = render_svg_section(bblocks, maxw, [COLORS, ECOLORS, DCOLORS], h=h,
-                                fsize=fsize, width=width, top=h1 + h, invert=True)
+                                fsize=fsize, width=width, top=h1, invert=True)
     return SVG.format('\n'.join(s1 + s2), width=width, height=h2)
 
 
-def render_fg(blocks, multiplier):
+def render_fg(blocks, multiplier, out):
     for b in blocks:
         trace = []
         for t in b['trace']:
             trace.append('{}:{}:{}'.format(*t))
 
-        print(';'.join(trace), round(b['ww'] * multiplier))
+        print(';'.join(trace), round(b['ww'] * multiplier), file=out)
+
+
+try:
+    import pytest
+
+    def pytest_addoption(parser):
+        group = parser.getgroup('flameprof')
+        group.addoption("--flameprof-svg", help="filename with out svg, default is %(default)s",
+                        default='/tmp/pytest-prof.svg')
+        group.addoption("--flameprof-cpu", action="store_true", help="ignore io wait")
+
+    def pytest_configure(config):
+        config.pluginmanager.register(PyTestPlugin(config.getvalue('flameprof_svg'),
+                                                   config.getvalue('flameprof_cpu')))
+
+    class PyTestPlugin(object):
+        def __init__(self, out, cpu):
+            self.out = out
+            self.any_test_was_run = False
+            if cpu:
+                self.profiler = Profile(time.clock)
+            else:
+                self.profiler = Profile()
+
+        @pytest.hookimpl(hookwrapper=True)
+        def pytest_runtest_call(self, item):
+            self.any_test_was_run = True
+            self.profiler.enable()
+            try:
+                yield
+            finally:
+                self.profiler.disable()
+
+        def pytest_unconfigure(self, config):
+            if self.any_test_was_run:
+                self.profiler.create_stats()
+                render(self.profiler.stats, open(self.out, 'w'))
+except ImportError:
+    pass
 
 
 SVG = '''\
@@ -228,29 +298,72 @@ ELEM = '''\
 </g></svg>'''
 
 
+def render(stats, out, fmt=DEFAULT_FORMAT, threshold=DEFAULT_THRESHOLD/100,
+           width=DEFAULT_WIDTH, row_height=DEFAULT_ROW_HEIGHT,
+           fsize=DEFAULT_FONT_SIZE, log_mult=DEFAULT_LOG_MULT):
+
+    funcs, calls = calc_callers(stats)
+    blocks, bblocks, maxw = prepare(funcs, calls, threshold=threshold)
+
+    if fmt == 'svg':
+        print(render_svg(blocks, bblocks, maxw, h=row_height,
+                         fsize=fsize, width=width), file=out)
+    elif fmt == 'log':
+        render_fg(blocks, log_mult, out)
+
+    out.flush()
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Make flamegraph from cProfile stats.')
-    parser.add_argument('stats', help='file with cProfile stats')
-    parser.add_argument('--width', type=int, help='image width, default is %(default)s', default=1200)
-    parser.add_argument('--row-height', type=int, help='row height, default is %(default)s', default=24)
-    parser.add_argument('--font-size', type=int, help='font size, default is %(default)s', default=12)
-    parser.add_argument('--threshold', type=float, default=0.1,
+    parser.add_argument('stats', help='file with cProfile stats or command to run')
+    parser.add_argument('--width', type=int, help='image width, default is %(default)s', default=DEFAULT_WIDTH)
+    parser.add_argument('--row-height', type=int, help='row height, default is %(default)s', default=DEFAULT_ROW_HEIGHT)
+    parser.add_argument('--font-size', type=int, help='font size, default is %(default)s', default=DEFAULT_FONT_SIZE)
+    parser.add_argument('--threshold', type=float, default=DEFAULT_THRESHOLD,
                         help='limit functions relative cumulative time in percents, default is %(default)s%%')
-    parser.add_argument('--format', choices=['svg', 'log'], default='svg',
+    parser.add_argument('--format', choices=['svg', 'log'], default=DEFAULT_FORMAT,
                         help='output format, default is %(default)s. `log` is suitable as input for flamegraph.pl')
-    parser.add_argument('--log-mult', type=int, default=1000000,
+    parser.add_argument('--log-mult', type=int, default=DEFAULT_LOG_MULT,
                         help='multiply score value for log format, default is %(default)s')
     parser.add_argument('--version', action='version', version='%(prog)s {}'.format(version))
+    parser.add_argument('-r', '--run', action='store_true', help='run python script')
+    parser.add_argument('-m', '--run-module', action='store_true', help='run python module')
+    parser.add_argument('--cpu', action='store_true', help='count cpu time only (without io wait)')
+    parser.add_argument('-o', '--out', type=argparse.FileType('w'), default=sys.stdout, help='file with final svg')
 
-    args = parser.parse_args()
+    args, rest = parser.parse_known_args()
 
-    s = pstats.Stats(args.stats)
-    funcs, calls = calc_callers(s.stats)
-    blocks, bblocks, maxw = prepare(funcs, calls, threshold=args.threshold / 100)
+    if args.run or args.run_module:
+        if args.run:
+            code = compile(open(args.stats, mode='rb').read(), '__main__', 'exec', dont_inherit=True)
+            fname = args.stats
+        elif args.run_module:
+            if PY2:
+                mod_name, loader, code, fname = runpy._get_module_details(args.stats)
+            else:
+                mod_name, mod_spec, code = runpy._get_module_details(args.stats)
+                fname = mod_spec.origin
 
-    if args.format == 'svg':
-        print(render_svg(blocks, bblocks, maxw, h=args.row_height,
-                         fsize=args.font_size, width=args.width))
+        if args.cpu:
+            s = Profile(time.clock)
+        else:
+            s = Profile()
 
-    if args.format == 'log':
-        render_fg(blocks, args.log_mult)
+        globs = {
+            '__file__': fname,
+            '__name__': '__main__',
+            '__package__': None,
+        }
+        sys.argv[:] = [fname] + rest
+        sys.path.insert(0, os.path.dirname(args.stats))
+        try:
+            s.runctx(code, globs, None)
+        except SystemExit:
+            pass
+        s.create_stats()
+    else:
+        s = pstats.Stats(args.stats)
+
+    render(s.stats, args.out, args.format, args.threshold / 100,
+           args.width, args.row_height, args.font_size, args.log_mult)
